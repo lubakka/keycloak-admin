55cfb00bcfb3361a4462afb9dbcb83ea355fa06f
Lyubomir Boykov <Lyubomir.Boykov@pro-samed.de>
2025-02-08 12:34:43 +0100

    KeycloakAdminManager added in services

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -2,7 +2,13 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Lubakka\KeycloakAdmin\KeycloakAdminManager;
+
 return function (ContainerConfigurator $container) {
     $parameters = $container->parameters();
     $services = $container->services();
+
+    $services
+        ->set('keycloak_admin_manager', KeycloakAdminManager::class)
+    ;
 };
